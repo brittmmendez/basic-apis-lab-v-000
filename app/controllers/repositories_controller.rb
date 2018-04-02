9a3b0bdef47b981e4da970f9f72ec6eb36373463
Brittany Morris <brittmmendez@gmail.com>
2018-04-02 22:33:56 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,15 @@
 class RepositoriesController < ApplicationController
 
   def search
+    query=params[:query]
+    url= 'https://api.github.com/search/repositories?q=${query}'
 
   end
 
   def github_search
+  @resp = Faraday.get 'https://api.github.com/search/repositories?q=' do |req|
+      req.params['query'] = params[:query]
+      render 'search'
+
   end
 end
